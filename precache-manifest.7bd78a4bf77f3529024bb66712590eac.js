993b3f14bbcb66a0eaa162abc341cabd57b5c4b3
tudordan7 <tudor.pop@decagon.tech>
2021-12-06 11:02:18 +0200

    Updates

---
+++ precache-manifest.7bd78a4bf77f3529024bb66712590eac.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4904cdce679520b3525044f19d7f755d",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "6b2953448b09c9d55b67",
+    "url": "/my-app/static/css/main.c59a10ec.chunk.css"
+  },
+  {
+    "revision": "cb2aa6b57c82d90a0606",
+    "url": "/my-app/static/js/2.0af2c267.chunk.js"
+  },
+  {
+    "revision": "4b276b96ab29f5da30c5717788208388",
+    "url": "/my-app/static/js/2.0af2c267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21880f0002064bddbd24",
+    "url": "/my-app/static/js/3.17d64dba.chunk.js"
+  },
+  {
+    "revision": "6b2953448b09c9d55b67",
+    "url": "/my-app/static/js/main.92e7f9c4.chunk.js"
+  },
+  {
+    "revision": "43173ba0366007f3e34b",
+    "url": "/my-app/static/js/runtime-main.6d70f8e8.js"
+  },
+  {
+    "revision": "bb83086fc555a7f043c7576a32ffe8ce",
+    "url": "/my-app/static/media/about.bb83086f.jpeg"
+  },
+  {
+    "revision": "bb84438ab9ab9e0da3ed3f6f56f6b532",
+    "url": "/my-app/static/media/about2.bb84438a.jpeg"
+  },
+  {
+    "revision": "134f423b313e7ef86d0e67eba05d00aa",
+    "url": "/my-app/static/media/annai.134f423b.png"
+  },
+  {
+    "revision": "fbfc8fdbe6ecaf73d08b35a4ddf8023f",
+    "url": "/my-app/static/media/jitsi.fbfc8fdb.png"
+  },
+  {
+    "revision": "45c8cae5ae86f6ff913a3ab6ec89bc19",
+    "url": "/my-app/static/media/mrrobot.45c8cae5.mp3"
+  },
+  {
+    "revision": "33b675d491bc71ba9d800c608ce013be",
+    "url": "/my-app/static/media/octant.33b675d4.png"
+  },
+  {
+    "revision": "373e77dcd490f55f400bac13747aba9b",
+    "url": "/my-app/static/media/timetex.373e77dc.png"
+  },
+  {
+    "revision": "915aa23401d7b1f81ba037f60034eb61",
+    "url": "/my-app/static/media/timetex1.915aa234.png"
+  }
+]);
